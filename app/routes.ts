469ceedbf844bf6f092796dbb43e1dfab010e3bb
wxndn <windy.pilierreyes@gmail.com>
2024-12-11 04:22:35 +0100

    Create routes.ts

---
+++ app/routes.ts
@@ -0,0 +1,16 @@
+export const routes = {
+  home: '/',
+  dictionary: '/dictionary',
+  forum: '/forum',
+  notes: '/notes',
+  practice: '/practice',
+  resources: '/resources',
+  career: '/career',
+  mentoring: '/mentoring',
+  progress: '/progress',
+  calendar: '/calendar',
+  quiz: '/quiz',
+  offline: '/offline',
+  settings: '/settings',
+  profile: '/profile'
+}
